b78d8fdb4dd9d7d4f9abe7840ad2bd4c3f068422
usualpn0221 <usualpn@gmail.com>
2019-05-28 23:30:12 +0900

    Third commit test

--- src/Application.php
+++ src/Application.php
@@ -53,9 +53,9 @@ class Application extends BaseApplication
          * Only try to load DebugKit in development mode
          * Debug Kit should not be installed on a production system
          */
-        // if (Configure::read('debug')) {
-        //     $this->addPlugin(\DebugKit\Plugin::class);
-        // }
+        if (Configure::read('debug')) {
+            $this->addPlugin(\DebugKit\Plugin::class);
+        }
     }
 
     /**
