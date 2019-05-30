1e87912737ec4e1cb7c76f3e3c7208dc801574eb
usualpn0221 <usualpn@gmail.com>
2019-05-31 01:58:49 +0900

    test

--- src/Application.php
+++ src/Application.php
@@ -53,10 +53,11 @@ class Application extends BaseApplication
          * Only try to load DebugKit in development mode
          * Debug Kit should not be installed on a production system
          */
-        if (Configure::read('debug')) {
-            $this->addPlugin(\DebugKit\Plugin::class);
-        }
+        // if (Configure::read('debug')) {
+        //     $this->addPlugin(\DebugKit\Plugin::class);
+        // }
     }
+
 
     /**
      * Setup the middleware queue your application will use.
